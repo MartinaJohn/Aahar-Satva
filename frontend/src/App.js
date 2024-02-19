6d555b5ce064e06b4f6ee729ab2aeadb33a16551
Martina John <martina07j@gmail.com>
2024-02-19 21:42:11 +0530

    Removed unnecessary code snippet

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Routes,Route } from 'react-router-dom'
-import Greeter from "./pages/Greeter"
+import Home from "./pages/Home"
 import Signup from './pages/Signup'
 import Signin from './pages/Signin'
 import Analyst from './pages/Analyst'
@@ -12,7 +12,7 @@ const App = () => {
     <div>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Greeter/>}/>
+        <Route path="/" element={<Home/>}/>
         <Route path="/signup" element={<Signup />}/>
         <Route path="/signin" element={<Signin />}/>
         <Route path="/analyst" element={<Analyst />}/>
